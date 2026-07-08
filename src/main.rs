c2bb4abdaf5aba76decf3343c4da274c56ab17ba
techs-sus <92276908+techs-sus@users.noreply.github.com>
2026-07-08 12:19:12 -0400

    develop: install sqlx and disable window bounds

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+// mod database;
+
 use gpui::{
-	App, Application, Bounds, Context, SharedString, Window, WindowBounds, WindowOptions, div,
-	prelude::*, px, rgb, size,
+	App, Application, Bounds, Context, Menu, MouseButton, SharedString, Window, WindowBounds,
+	WindowOptions, div, prelude::*, px, rgb, size,
 };
 
 struct HelloWorld {
@@ -8,13 +10,13 @@ struct HelloWorld {
 }
 
 impl Render for HelloWorld {
-	fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
+	fn render(&mut self, window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
 		div()
 			.flex()
 			.flex_col()
 			.gap_3()
 			.bg(rgb(0x505050))
-			.size(px(500.0))
+			.size(window.viewport_size().width)
 			.justify_center()
 			.items_center()
 			.shadow_lg()
@@ -22,6 +24,11 @@ impl Render for HelloWorld {
 			.border_color(rgb(0x0000ff))
 			.text_xl()
 			.text_color(rgb(0xffffff))
+			.on_any_mouse_down(|event, window, app| {
+				if matches!(event.button, MouseButton::Left) {
+					app.quit();
+				};
+			})
 			.child(format!("Hello, {}!", &self.text))
 			.child(
 				div()
@@ -39,10 +46,10 @@ impl Render for HelloWorld {
 
 fn main() {
 	Application::new().run(|cx: &mut App| {
-		let bounds = Bounds::centered(None, size(px(500.), px(500.0)), cx);
+		// let bounds = Bounds::centered(None, size(px(500.), px(500.0)), cx);
 		cx.open_window(
 			WindowOptions {
-				window_bounds: Some(WindowBounds::Windowed(bounds)),
+				// window_bounds: Some(WindowBounds::Windowed(bounds)),
 				..Default::default()
 			},
 			|_, cx| {
@@ -54,4 +61,3 @@ fn main() {
 		.unwrap();
 	});
 }
-
