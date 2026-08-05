6b9b1f86464c8682ed5ed000405cf0268e21cb07
techs-sus <92276908+techs-sus@users.noreply.github.com>
2026-08-05 18:38:45 -0400

    feature: add m3a writing, position data, basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::{
 	collections::HashSet,
+	io::{BufWriter, Write},
 	path::{Path, PathBuf},
 };
 
+use clap::Parser;
 use reqwest::{
 	Client,
 	header::{CONTENT_TYPE, USER_AGENT},
@@ -46,6 +48,7 @@ pub const SQLITE_APPLICATION_ID: u32 = 0x7D8A4B83;
 // - playlist.db
 /// A playlist on disk.
 struct Playlist {
+	name: String,
 	folder: PathBuf,
 	db: Connection,
 	client: YouTubeMusicClient,
@@ -58,10 +61,12 @@ impl Playlist {
 			return Err(Error::PlaylistHasNoParentFolder);
 		};
 
+		let folder = tokio::fs::canonicalize(folder).await?;
+
 		tokio::fs::create_dir(folder.join("audio")).await?;
 		tokio::fs::create_dir(folder.join("thumbnail")).await?;
 
-		let db = Connection::open(path)?;
+		let db = Connection::open(&path)?;
 
 		// https://sqlite.org/pragma.html#pragma_application_id
 		let application_id = db.pragma_query_value(
@@ -103,6 +108,7 @@ impl Playlist {
               thumbnail_path text,
 
               youtube_video_id text NOT NULL UNIQUE,
+							position integer NOT NULL UNIQUE,
     				);
     				COMMIT;
 				",
@@ -129,6 +135,13 @@ impl Playlist {
 			folder,
 			client: YouTubeMusicClient::new(),
 			reqwest_client: Client::new(),
+			name: path
+				.as_ref()
+				.with_extension("")
+				.file_name()
+				.expect("playlist should have name")
+				.to_string_lossy()
+				.to_string(),
 		})
 	}
 
@@ -191,14 +204,21 @@ impl Playlist {
 			.join(video_id)
 			.with_extension(stream_info.format.extension());
 
-		let response = self
-			.reqwest_client
-			.get(stream_info.url)
-			.header(USER_AGENT, &stream_info.user_agent)
-			.send()
-			.await?;
+		// if the file was already downloaded, don't refetch it
+		match tokio::fs::try_exists(&audio_path).await {
+			Ok(true) => {}
 
-		tokio::fs::write(&audio_path, response.bytes().await?).await?;
+			_ => {
+				let response = self
+					.reqwest_client
+					.get(stream_info.url)
+					.header(USER_AGENT, &stream_info.user_agent)
+					.send()
+					.await?;
+
+				tokio::fs::write(&audio_path, response.bytes().await?).await?;
+			}
+		};
 
 		Ok((audio_path, stream_info.user_agent))
 	}
@@ -238,9 +258,6 @@ impl Playlist {
 			tx.commit()?;
 		}
 
-		// TODO: is asking the database for which ids don't exist faster or slower?
-		// TODO: would that lead to overhead? because tracks are already allocated once
-
 		let already_existing_track_ids = self
 			.db
 			.prepare(
@@ -253,20 +270,30 @@ impl Playlist {
 			.filter_map(Result::ok)
 			.collect::<HashSet<_>>();
 
+		// clean temporary table to increase performance
+		self.db.execute("DELETE FROM incoming_tracks;", [])?;
+
 		let mut insert_track_statement = self.db.prepare(
 			"
-			INSERT INTO tracks (title, audio_path, thumbnail_path, youtube_video_id) VALUES (?1, ?2, ?3, ?4)
+			INSERT INTO tracks (title, audio_path, thumbnail_path, position, youtube_video_id) VALUES (?1, ?2, ?3, ?4, ?5)
 			ON CONFLICT (youtube_video_id) DO UPDATE SET
 				title = excluded.title,
 				audio_path = excluded.audio_path,
 				thumbnail_path = excluded.thumbnail_path;
+				position = excluded.position;
 			",
 		)?;
 
-		for track in tracks {
+		let mut ensure_track_has_updated_position = self
+			.db
+			.prepare("UPDATE tracks SET position = ?1 WHERE youtube_video_id = ?2")?;
+
+		for (playlist_ordered_position, track) in tracks.into_iter().enumerate() {
 			let youtube_video_id = track.id.key();
 
 			if already_existing_track_ids.contains(&*youtube_video_id) {
+				// update track position as it may have changed in the upstream
+				ensure_track_has_updated_position.execute((playlist_ordered_position, youtube_video_id))?;
 				continue;
 			}
 
@@ -278,37 +305,145 @@ impl Playlist {
 				.await
 				.unwrap_or(None);
 
+			// all database paths must be relative for portability
+			let resulting_audio_path = pathdiff::diff_paths(resulting_audio_path, &self.folder).unwrap();
+			let thumbnail_path = thumbnail_path
+				.map(|path| pathdiff::diff_paths(path, &self.folder))
+				.unwrap();
+
 			insert_track_statement.execute((
 				track.title,
 				resulting_audio_path.to_string_lossy().to_string(),
 				thumbnail_path.map(|path| path.to_string_lossy().to_string()),
+				playlist_ordered_position,
 				youtube_video_id,
 			))?;
 		}
 
-		// clean temporary table to increase performance
-		self.db.execute("DELETE FROM incoming_tracks;", [])?;
+		Ok(())
+	}
+
+	fn write_playlist_to_m3a(&self) -> Result<(), Error> {
+		struct RegularTrack {
+			path: PathBuf,
+			title: String,
+		}
+
+		let mut prepared = self
+			.db
+			.prepare("SELECT audio_path, title FROM tracks ORDER BY position ASC")?;
+
+		let tracks = prepared.query_map((), |row| {
+			Ok(RegularTrack {
+				path: self.folder.join(row.get::<_, String>(0)?),
+				title: row.get::<_, String>(1)?,
+			})
+		})?;
+
+		let mut buf = BufWriter::new(std::fs::File::create(
+			self.folder.join(&self.name).with_extension("m3a"),
+		)?);
+
+		write!(buf, "#EXTM3U\n")?;
+
+		for track in tracks.into_iter().filter_map(Result::ok) {
+			write!(buf, "#EXTINF:0,{}\n", track.title)?;
+			write!(buf, "{}\n", track.path.to_string_lossy())?;
+		}
+
+		buf.flush()?;
+
+		Ok(())
+	}
+
+	fn set_upstream(&self, youtube_playlist_id: &str) -> Result<(), Error> {
+		self.db.execute(
+			"INSERT INTO playlist_metadata (singleton_key, youtube_playlist_id) VALUES (1, ?1) ON CONFLICT(singleton_key) DO UPDATE SET youtube_playlist_id = excluded.youtube_playlist_id",
+			[youtube_playlist_id],
+		)?;
 
 		Ok(())
 	}
 }
 
+#[derive(clap::Args)]
+struct PlaylistOptions {
+	/// The sqlite database file used to sync.
+	#[arg(short = 'p', long = "playlist")]
+	playlist_db_path: PathBuf,
+}
+
+#[derive(clap::Subcommand)]
+enum Command {
+	/// Initializes a playlist file.
+	Init {
+		#[clap(flatten)]
+		options: PlaylistOptions,
+
+		/// The upstream playlist to init this local one.
+		#[arg(short = 'u', long = "upstream")]
+		youtube_upstream_playlist: String,
+	},
+
+	/// Syncs music from a playlist file and produces an m3u8 file.
+	Sync {
+		#[clap(flatten)]
+		options: PlaylistOptions,
+	},
+
+	/// Writes a playlist's m3a file.
+	WriteToM3a {
+		#[clap(flatten)]
+		options: PlaylistOptions,
+	},
+}
+
+#[derive(clap::Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+	#[command(subcommand)]
+	command: Command,
+}
+
 #[tokio::main]
 async fn main() {
-	let playlist_id = "OLAK5uy_nFiS1SeXBnJII-kBfpg7kGRB0JeE_tot8";
-	let client = YouTubeMusicClient::new();
-	let entries = client
-		.get_playlist_entries(playlist_id)
-		.await
-		.expect("failed getting entries of playlist");
+	// let playlist_id = "OLAK5uy_nFiS1SeXBnJII-kBfpg7kGRB0JeE_tot8";
+	let args = Args::parse();
+	match args.command {
+		Command::Init {
+			options,
+			youtube_upstream_playlist,
+		} => {
+			let playlist = Playlist::from_path(options.playlist_db_path)
+				.await
+				.expect("failed opening playlist");
 
-	let first_track = entries.first().expect("no first entry");
+			playlist
+				.set_upstream(&youtube_upstream_playlist)
+				.expect("failed setting playlist upstream");
 
-	let video_id = first_track.id.key();
-	let stream = client
-		.get_stream(&video_id)
-		.await
-		.expect("failed getting stream");
-	println!("stream: {stream:?}");
-	println!("cover: {:?}", first_track.cover)
+			println!("successfully set playlist upstream to {youtube_upstream_playlist}");
+		}
+		Command::Sync { options } => {
+			let mut playlist = Playlist::from_path(options.playlist_db_path)
+				.await
+				.expect("failed opening playlist");
+
+			playlist
+				.sync_from_youtube()
+				.await
+				.expect("failed syncing from youtube");
+			println!("successfully synced from youtube");
+		}
+		Command::WriteToM3a { options } => {
+			let playlist = Playlist::from_path(options.playlist_db_path)
+				.await
+				.expect("failed opening playlist");
+
+			playlist
+				.write_playlist_to_m3a()
+				.expect("failed writing playlist to m3a");
+			println!("successfully wrote playlist m3a");
+		}
+	};
 }
